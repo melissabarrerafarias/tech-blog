c6ecffb35b68a02ae1bd624ff6ea43e2c2def1ac
melissikiss <melissikiss@gmail.com>
2021-01-06 12:23:25 -0800

    fixed PUT error for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,25 +42,25 @@ router.post('/', (req, res) => {
 })
 
 // PUT /api/users/1
-// router.put('/:id', (req, res) => {
-//     User.update(req.body, {
-//         individualHooks: true,
-//         where: {
-//             id: req.body.id
-//         }
-//     })
-//         .then(dbUserData => {
-//             if (!dbUserData[0]) {
-//                 res.status(404).json({ message: 'No user found with this id' });
-//                 return;
-//             }
-//             res.json(dbUserData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
