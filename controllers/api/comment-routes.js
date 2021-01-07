c4eb07f8938c0f12c218a50b7d9ab2824fbcd6de
melissikiss <melissikiss@gmail.com>
2021-01-06 18:18:09 -0800

    created comment model and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router(); 
+const { Comment } = require('../../models'); 
+
+// /api/comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+        attributes: [
+            'id',
+            'comment_text',
+            'user_id',
+            'post_id',
+        ],
+    }).then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// api/comments
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+});
+
+// api/comments/1
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'Sorry, no comment found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
